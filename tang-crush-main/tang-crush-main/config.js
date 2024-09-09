a9610c9b45f26973cd0ce7513f1b6ae0f4384391
ManhHung1303 <manhhunghm13032005@gmail.com>
2024-09-09 17:19:56 +0700

    upload  lần 2

---
+++ tang-crush-main/tang-crush-main/config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Babe à!",
+  introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+  btnIntro: "Nhấn vào đây có đặc biệt phía sau nè bé ",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙",
+  desc: "Phải chăng em đã ấn tượng anh  ngay từ lần đầu nhắn tin với anh   ",
+  btnYes: "Vẫn  rất thích anh <33333",
+  btnNo: "Không,Anh à :3",
+  question: "Trên thế giới hơn 7 tỉ người nhưng  anh lại yêu em mất rùi  <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Em cũng rất yêu anh  <33333333",
+  mess: "Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘",
+  messDesc: "Ngày 14/9 anh qua đón bé đi chơi với anh  nhé Babe ",
+  btnAccept: "Em đồng ý  ạ <<<<3",
+  messLink: "https://www.facebook.com/messages/e2ee/t/8234546876612629", //link mess của các bạn. VD: https://m.me/nam.nodemy
+};
